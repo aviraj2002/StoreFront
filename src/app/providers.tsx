68b43b2ceea2fe21cdbd0f0ab5b424523c8dee85
Avi Raj <abhibarnwalsmd@gmail.com>
2025-12-02 09:27:00 +0000

    File changes

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import { CartProvider } from "@/context/cart-context";
+import { FirebaseClientProvider }from "@/firebase/client-provider";
 import type { ReactNode } from "react";
 
 export function Providers({ children }: { children: ReactNode }) {
-  return <CartProvider>{children}</CartProvider>;
+  return (
+    <FirebaseClientProvider>
+      <CartProvider>{children}</CartProvider>
+    </FirebaseClientProvider>
+  );
 }
